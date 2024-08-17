9ccde0725a1322a9ee564e61ad214fbc3446aaec
ikennekwuo-github <ikeemmanuellike@gmail.com>
2024-08-18 01:22:31 +0530

    solution_assignment

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# def output(name, age, location):
+#     return f"Name: {name}, Age: {age}, Location: {location}"
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Ask user for their age
+age = input("How old are you? ")
+
+# Ask the user for location
+location = input("Where are you? ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
